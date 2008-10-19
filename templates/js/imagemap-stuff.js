8c528e2670899567d2c53e07313097efb1905ea7
tolleiv <tolleiv@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-10-19 15:15:48 +0000

    some refactoring and GUI-performance improvements...
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/imagemap_wizard/trunk@13139 735d13b6-9817-0410-8766-e36946ffe9aa

--- templates/js/imagemap-stuff.js
+++ templates/js/imagemap-stuff.js
@@ -1,10 +1,18 @@
 
 //TODO validate coords
-//TODO implement colorpicker
 
 canvasClass = function () {
     var canvasId,canvasVectors,pictureId,boxId,formsId,boxMarkerCount,areaCount,areaObjects,areaObjectList,formBlueprints;
-
+    
+    /**
+    *  Initialize basic-js Object which handles all the functionality
+    * 
+    * @param id     container which is supposed to contain all canvas-layers
+    * @param picid  container which containes the picture itself
+    * @param boxid  container which holds the Markerspoints
+    * @param formid container which holds form-blueprints and which is supposed to containe the real forms aswell
+    * @usage  external
+    */
     this.init = function (id,picid,boxid,formid){
         canvasId = "#" + id;
         pictureId = "#" + picid;
@@ -12,33 +20,59 @@ canvasClass = function () {
         formsId = "#" + formid;
         boxMarkerCount = 0;
         areaCount = 0;
-        canvasVectors = new jsGraphics(id);
+        canvasVectors = new Array();
         areaObjects = new Array();
         areaObjectList = new Array();
 
         formBlueprints = this.parseFormToBluePrint(formsId);
         $(formsId).empty();
-        //TODO: find out where to put this :P
-       // $(formsId).append("<span class=\"cc\"><!-- --></span>");
         $(canvasId).width($(pictureId).width()).height($(pictureId).height());
         $(boxId).width($(pictureId).width()+6).height($(pictureId).height()+6);
     }
 
-    // called from outside...
+    /**
+    *  Adds a Rectangle-Area Object
+    * 
+    * @param coords initial coordinates
+    * @param linkValue  typolink values
+    * @param colorValue the hex-value of the color
+    * @usage external
+    */
     this.addRectArea = function (coords,linkValue,colorValue) {
 		var tmp = new areaRectClass();
         var theId = this.getNextId();
         tmp.init(this,theId,coords,linkValue,colorValue);
         areaObjects[theId] = tmp;
         areaObjectList.push(theId);
+        this.addCanvasLayer(theId);
         $(formsId).append(tmp.formMarkup().replace(/OBJID/g,theId));
         $(formsId).sortable({});
 		areaObjects[theId].applyBasicAreaActions($("#" + theId));
-        this.updateCanvas();
+        this.updateCanvas(theId);
         this.updateForm(tmp.getId());
     }
+    
+    /**
+    *  Adds a Rectangle-Area Object
+    * 
+    * @param id the area-id which should be removed
+    * @usage area*Classes
+    */ 
+    this.removeArea = function (id) {
+        var tmpArr = new Array();
+        jQuery.each(areaObjectList, function(i, objId) {
+            if(objId!=id) { tmpArr.push(objId); }
+        });
+        areaObjectList=tmpArr;
+        this.removeCanvasLayer(id);
+    }
 
-	// called from outside
+    /**
+    *  Creates valid XML from the current Area-Objects
+    *
+    * @returns XML-String
+    * @usage external
+    */
 	this.persistanceXML = function() {
 		var result = "";
         var tmpArr = new Array();
@@ -47,62 +81,101 @@ canvasClass = function () {
         });
         return result;
 	}
-    // called from area*Class
-    this.removeArea = function (id) {
-        var tmpArr = new Array();
-        jQuery.each(areaObjectList, function(i, objId) {
-            if(objId!=id) { tmpArr.push(objId); }
-        });
-        areaObjectList=tmpArr;
-        this.updateCanvas();
-    }
 
-    // called from area*Class
-    this.addMarkerPoint = function(obj,id,x,y) {
-        var markerId = this.getNextBoxMarkerId();
+
+    /**
+    *  Adds a draggable MarkerPoint which is used to resize the Areas etc..
+    *
+    * @param id     the Area-Object-Id needed as back-reference
+    * @param x      initial x-coord
+    * @param y      initial y-coord
+    * @returns String   the new container-id of the created marker-point
+    * @usage area*Classes
+    */
+    this.addMarkerPoint = function(id,x,y) {
+        var markerId = this.getNextMarkerPointId();
         $(boxId).append("<div class=\"point\" id=\"" + markerId + "\" style=\"left:"+ x +"px;top:"+ y +"px;\"><!-- --></div>");
-        $("#" + markerId).data("canvas",obj);
+        $("#" + markerId).data("canvas",this);
         $("#" + markerId).data("parentId",id);
 
         $("#" + markerId).draggable({
             containment:'parent',
             refreshPositions:true,
-/*
+
             drag:function(e,ui) {
-            	$(this).data("canvas").updateCanvas();
+            	$(this).data("canvas").updateCanvas($(this).data("parentId"));
             	$(this).data("canvas").updateForm($(this).data("parentId"));
             },
-*/
-//            start:function(e,ui) { },
-            start:function(e,ui) {
-                $(this).everyTime(40,"canvasFn", function() {
-                    $(this).data("canvas").updateCanvas();
+
+            start:function(e,ui) { },
+            /*start:function(e,ui) {
+                $(this).everyTime(5,"canvasFn", function() {
+                    $(this).data("canvas").updateCanvas($(this).data("parentId"));
                     $(this).data("canvas").updateForm($(this).data("parentId"));
                 }, 0 , false)
 
-            },
+            },*/
 
             stop:function(e,ui) {
                 $(this).stopTime("canvasFn");
-                $(this).data("canvas").updateCanvas();
+                $(this).data("canvas").updateCanvas($(this).data("parentId"));
                 $(this).data("canvas").updateForm($(this).data("parentId"));
             }
         });
         return "#" + markerId;
     }
-    // called from area*Class
+
+    /**
+    *  Removes Markerpoints
+    *
+    * @param id     the container-id
+    * @usage area*Classes
+    */
     this.removeMarkerPoint = function(id) {
         $(id).remove();
     }
 
-    // called from area*Class
-    this.updateCanvas = function() {
-        canvasVectors.clear();
-        jQuery.each(areaObjectList, function(i, objId) {
-            areaObjects[objId].drawSelection(canvasVectors);
-        });
-        canvasVectors.paint();
+    /**
+    * Add a new canvas-layer and create a new Graphics-Objects.
+    * This keeps canvases separate and adds to overall performance.
+    *
+    * @param id     the canvasId
+    * @usage internal
+    */
+    this.addCanvasLayer = function(id) {
+        $(canvasId).append('<div id="' + id + '_canvas" class="canvas"><!-- --></div>');
+        canvasVectors[id] = new jsGraphics(id + '_canvas');
+    } 
+
+    /**
+    *  Re-Paint a canvas-layer for a single Area-Object.
+    *
+    * @param id     the object-id
+    * @usage area*Classes
+    */
+    this.updateCanvas = function(id) {        
+            canvasVectors[id].clear();
+            areaObjects[id].drawSelection(canvasVectors[id]);
+            canvasVectors[id].paint();
     }
+
+    /**
+    * Remove a canvas-layer and make sure that nothing is displayed anymore.
+    *
+    * @param id     the canvasId
+    * @usage internal
+    */    
+    this.removeCanvasLayer = function(id) {
+        canvasVectors[id].clear();
+        $('#' + id + '_canvas').remove();
+    }
+    
+    /**
+    * Re-Sync the form-data with the Area-Object
+    *
+    * @param id     the object-id
+    * @usage area*Classes
+    */    
     this.updateForm = function(id) {
         var data = areaObjects[id].formUpdate();
         jQuery.each(data.split(";"), function(elem, value) {
@@ -110,7 +183,13 @@ canvasClass = function () {
             $("#" + item[0]).attr("value",item[1]);
         });
     }
-
+    
+    /**
+    * Reload form from blueprint after the linkvalue was updated (Required since Link-Wizard URL need to change).
+    *
+    * @param id     the object-id
+    * @usage external
+    */ 
 	this.triggerAreaLinkUpdate = function(id)  {
 		areaObjects[id].updateStatesFromForm();
         $("#" + areaObjects[id].getFormId()).replaceWith(areaObjects[id].formMarkup().replace(/OBJID/g,id));
@@ -118,18 +197,40 @@ canvasClass = function () {
         this.updateForm(areaObjects[id].getId());
 	}
 
+    /**
+    * Generate a new object-id
+    *
+    * @usage internal
+    */ 
     this.getNextId = function() {
         areaCount = areaCount + 1;
         return "Object" + areaCount;
     }
-    this.getNextBoxMarkerId = function() {
+
+    /**
+    * Generate a new markerpoint-id
+    *
+    * @usage internal
+    */ 
+    this.getNextMarkerPointId = function() {
         boxMarkerCount = boxMarkerCount + 1;
         return "markerPoint" + boxMarkerCount
     }
-
-    // called from area*Class
+    
+    /**
+    * Provides access to the form-blueprints
+    *
+    * @param id     the form-id
+    * @usage area*Classes
+    */ 
     this.getFormBlueprint = function(id) { return formBlueprints[id]; }
-
+    
+    /**
+    * Parses form-blueprints which contain the basic structur of the different Area-Forms.
+    *
+    * @param id     the form-id
+    * @usage internal
+    */ 
     this.parseFormToBluePrint = function(id) {
         var result = new Array();
         $(id + " > div").each(function(elem) {
@@ -163,8 +264,8 @@ areaRectClass = function () {
         _moreOptionsVisible = false;
     	var tmpcoords = coords.split(",");
     	markers = new Array();
-        markers.push(canvasObj.addMarkerPoint(canvasObj,_id,tmpcoords[0],tmpcoords[1]));
-        markers.push(canvasObj.addMarkerPoint(canvasObj,_id,tmpcoords[2],tmpcoords[3]));
+        markers.push(canvasObj.addMarkerPoint(_id,tmpcoords[0],tmpcoords[1]));
+        markers.push(canvasObj.addMarkerPoint(_id,tmpcoords[2],tmpcoords[3]));
     }
 
     // called from canvasClass
@@ -253,7 +354,7 @@ areaRectClass = function () {
   		        _color = color;
 		        $("#" + this.getFormId() + "_main > .colorPreview > div").css("backgroundColor", color);
 		        $("#" + this.getFormId() + "_color > .colorBox > div").css("backgroundColor", color);
-		        if(realChange==1) canvasObj.updateCanvas();
+		        if(realChange==1) canvasObj.updateCanvas(this.getId());
     }
 
     // called from canvasClass
@@ -263,7 +364,7 @@ areaRectClass = function () {
     this.updateCoordsFromForm = function(id) {
         this.setX(parseInt($("#" + this.getFormId() + "_x1").val()), parseInt($("#" + this.getFormId() + "_x2").val()));
         this.setY(parseInt($("#" + this.getFormId() + "_y1").val()), parseInt($("#" + this.getFormId() + "_y2").val()));
-        canvasObj.updateCanvas();
+        canvasObj.updateCanvas(this.getId());
     }
 
     // called from canvasClass
