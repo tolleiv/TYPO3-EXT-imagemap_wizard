e1212197d5e9a3392793d484b1ab683e36efd810
tolleiv <tolleiv@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-12-12 20:32:42 +0000

    added typo3 and php version constraints
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/imagemap_wizard/trunk@14873 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,6 +34,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
+            'php' => '5.0.0-5.2.99',
+            'typo3' => '3.8.0-4.2.99',
 			'cms' => '0.0.0',
 			'css_styled_content' => '0.0.0',
 		),
