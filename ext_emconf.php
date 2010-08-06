79f5e6e52e68f3ef574c9d42b298403ac91abcf5
tolleiv <tolleiv@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-08-06 08:44:33 +0000

    raised constraints to fit to latest PHP and TYPO3 versions
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/imagemap_wizard/trunk@36422 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,8 +34,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.0.0-5.2.99',
-			'typo3' => '3.8.0-4.3.99',
+			'php' => '5.0.0-5.3.99',
+			'typo3' => '3.8.0-4.4.99',
 			'cms' => '0.0.0',
 			'css_styled_content' => '0.0.0',
 		),
